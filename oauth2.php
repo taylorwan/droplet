3723fb941fcfbd4ed97324147698915183bb993b
EC2 Default User <ec2-user@ip-172-31-27-128.us-west-2.compute.internal>
2015-02-08 12:46:28 +0000

    clean up

--- oauth2.php
+++ oauth2.php
@@ -1,38 +1,61 @@
 <?php
-require_once "dropbox-sdk-php-1.1.4/lib/Dropbox/autoload.php";
-use \Dropbox as dbx;
-session_start();
+	require_once "dropbox-sdk-php-1.1.4/lib/Dropbox/autoload.php";
+	use \Dropbox as dbx;
+	session_start(); ?>
 
-$appInfo = dbx\AppInfo::loadFromJsonFile('credentials.json');
-$clientIdentifier = "Droplet/1.0";
-$redirectUri = "https://localhost/oauth2_pt2.php";
-// $csrfTokenStore = 'randomdigits';
-$csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
+<!DOCTYPE html>
 
-// echo $redirectUri;
-$webAuth = new dbx\WebAuthNoRedirect($appInfo, $clientIdentifier);
+<html>
 
-$_SESSION['webAuth'] = $webAuth;
+<head>
+<title>Droplet - OAuth 2.0</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style-main.css">
+</head>
 
-// $webAuth = new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore);
+<body class="oauth">
+	<aside class="sidenav">
+		<a href="../index.html" class="logo">
+			<span class="title">Droplet</span>
+			<img src="droplet-logo.png" />
+		</a>
+		<ul class="links">
+			<li><a href="../index.html">Home</a></li>
+			<li><a href="../docs.html">Docs</a></li>
+			<li><a href="#">Setup</a></li>
+		</ul>
+	</aside>
+	<div class="container">
+		<h1>Droplet - OAuth 2.0</h1>
+		<p class="lead">An SMS interactive tool for Dropbox.</p>
 
-$authorizeUrl = $webAuth->start();
+		<?php
 
-echo "go to " . $authorizeUrl;
+			$appInfo = dbx\AppInfo::loadFromJsonFile('credentials.json');
+			$clientIdentifier = "Droplet/1.0";
+			$redirectUri = "https://localhost/oauth2_pt2.php";
+			$csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
 
-echo "
-	<form method='POST' action='oauth2_pt2.php'>
-		Code: <input type='text' name='code'></input>
-		Number: <input type='text' name='phoneNumber'></input>
-		<input type='submit'>
-	</form>";
-	
+			$webAuth = new dbx\WebAuthNoRedirect($appInfo, $clientIdentifier);
 
-// echo $authorizeUrl;
-//header("Location: " . $authorizeUrl);
+			$_SESSION['webAuth'] = $webAuth;
+
+			$authorizeUrl = $webAuth->start();
+
+			echo "<a style=\"color:#fff\" href=\"$authorizeUrl\">Click me to find your code!</a>";
+
+			echo "
+				<form method='POST' action='oauth2_pt2.php'>
+					<input type='text' name='code' class='form-control' placeholder='code'></input>
+					<input type='text' name='phoneNumber' class='form-control' placeholder='phone #'></input>
+					<input type='submit'>
+				</form>";
+				
 
 
+		?>
+	</div>
 
+</body>
 
-
-?>
+</html>
--- oauth2_pt2.php
+++ oauth2_pt2.php
@@ -6,28 +6,41 @@ session_start();
 
 
 $code = $_POST['code'];
-$phoneNumber = $_POST['phoneNumber'];
+$phoneNumber = filter_var($_POST['phoneNumber'], FILTER_SANITIZE_NUMBER_INT);
 
 $webAuth = $_SESSION['webAuth'];
 
-echo $code;
-
-$dbHandler = new DBHandler();
-
-$dbHandler->insertAccessToken($phoneNumber, $code);
-
 try {
-   list($accessToken, $userId) = $webAuth->finish($code);
+  list($accessToken, $userId) = $webAuth->finish($code);
+	$dbxClient = new dbx\Client($accessToken, "Droplet/1.0");
+	$accountInfo = $dbxClient->getAccountInfo();
 }
 catch (dbx\Exception $ex) {
    print("Error communicating with Dropbox API: " . $ex->getMessage() . "\n");
 }
 
-$dbxClient = new dbx\Client($accessToken, "Droplet/1.0");
-$accountInfo = $dbxClient->getAccountInfo();
+echo "<br><br>";
 print_r($accountInfo);
 
+$numsTokensFileName = "/var/www/html/droplet/numbersToTokens.txt";
+$fileContents = file_get_contents($numsTokensFileName);
 
+if (!is_writeable($numsTokensFileName)){
+	echo "is not  writeable<br>";
+}
 
+if (strpos($fileContents, $phoneNumber) != FALSE  ){
+	echo "already in file";
+}else{
+	$newRecord = "{$phoneNumber}|{$accessToken}\n";
+	echo "putting $newRecord into $numsTokensFileName";
+	echo "res: " . file_put_contents($numsTokensFileName, $newRecord, FILE_APPEND | LOCK_EX); 
+}
+
+echo "Success! You can now text 443-364-3176 to use Droplet!";
 
 ?>
+
+
+
+
